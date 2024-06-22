34d499aa8317f3a4fcfdb4060844acc9359e7429
Mujtaba-Ali21 <mujtabaaliprince730@gmail.com>
2024-06-22 10:22:04 +0500

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -129,7 +129,7 @@ function Home() {
       <div className="new-arrivals container text-center mt-2 mb-3">
         <h2 className="">New Arrival</h2>
         <div className="row justify-content-center mt-4">
-          <div className="col-md-3 mb-4">
+          <div className="col-sm-6 col-md-6 col-lg-3 mb-4">
             <div className="card border-0 p-2">
               <img
                 src={exampleimg}
@@ -142,7 +142,7 @@ function Home() {
               </div>
             </div>
           </div>
-          <div className="col-md-3 mb-4">
+          <div className="col-sm-6 col-md-6 col-lg-3 mb-4">
             <div className="card border-0 p-2">
               <img
                 src={carousel_3}
@@ -155,7 +155,7 @@ function Home() {
               </div>
             </div>
           </div>
-          <div className="col-md-3 mb-4">
+          <div className="col-sm-6 col-md-6 col-lg-3 mb-4">
             <div className="card border-0 p-2">
               <img
                 src={carousel_2}
@@ -168,7 +168,7 @@ function Home() {
               </div>
             </div>
           </div>
-          <div className="col-md-3 mb-4">
+          <div className="col-sm-6 col-md-6 col-lg-3 mb-4">
             <div className="card border-0 p-2">
               <img
                 src={carousel_1}
@@ -187,95 +187,105 @@ function Home() {
       <div className="feature-collection container mt-2 mb-3">
         <h2 className="">Feature Collection</h2>
         <div className="row justify-content-center mt-4">
-          <div className="col-md-2 mb-4">
-            <div className="card border-0 p-2">
-              <img
-                src={exampleimg}
-                className="card-img-top card-img-custom"
-                alt="..."
-              />
-              <div className="card-body p-2">
-                <h5 className="card-title text-start">Card title</h5>
-                <p className="card-text text-start">Rs 19.99 PKR</p>
+          {Array(5)
+            .fill()
+            .map((_, idx) => (
+              <div className="col-sm-6 col-md-4 col-lg-3 mb-4" key={idx}>
+                <div className="card border-0 p-2">
+                  <img
+                    src={exampleimg}
+                    className="card-img-top card-img-custom"
+                    alt="..."
+                  />
+                  <div className="card-body p-2">
+                    <h5 className="card-title text-start">Card title</h5>
+                    <p className="card-text text-start">Rs 19.99 PKR</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="col-md-2 mb-4">
-            <div className="card border-0 p-2">
-              <img
-                src={exampleimg}
-                className="card-img-top card-img-custom"
-                alt="..."
-              />
-              <div className="card-body p-2">
-                <h5 className="card-title text-start">Card title</h5>
-                <p className="card-text text-start">Rs 19.99 PKR</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2 mb-4">
-            <div className="card border-0 p-2">
-              <img
-                src={exampleimg}
-                className="card-img-top card-img-custom"
-                alt="..."
-              />
-              <div className="card-body p-2">
-                <h5 className="card-title text-start">Card title</h5>
-                <p className="card-text text-start">Rs 19.99 PKR</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2 mb-4">
-            <div className="card border-0 p-2">
-              <img
-                src={exampleimg}
-                className="card-img-top card-img-custom"
-                alt="..."
-              />
-              <div className="card-body p-2">
-                <h5 className="card-title text-start">Card title</h5>
-                <p className="card-text text-start">Rs 19.99 PKR</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-2 mb-4">
-            <div className="card border-0 p-2">
-              <img
-                src={exampleimg}
-                className="card-img-top card-img-custom"
-                alt="..."
-              />
-              <div className="card-body p-2">
-                <h5 className="card-title text-start">Card title</h5>
-                <p className="card-text text-start">Rs 19.99 PKR</p>
-              </div>
-            </div>
-          </div>
+            ))}
         </div>
       </div>
 
-      <div className="premium-item container mt-2 mb-3">
+      <div className="premium-item container-md mt-2 mb-3">
         <div className="row justify-content-center mt-4">
-          <div className="col-lg-10 mb-4">
+          <div className="col-12 mb-4">
             <div className="card border-0 p-1">
-              <div className="d-flex align-items-center">
+              <div className="d-lg-flex align-items-lg-center">
                 <img
                   src={premium_item}
-                  className="img-fluid me-3 rounded"
+                  className="img-custom me-3 rounded"
                   alt="..."
-                  style={{ width: "50%", height: "auto" }}
                 />
-                <div className="card-body p-2">
-                  <h5 className="card-title text-center">Card title</h5>
-                  <p className="card-text text-center">Rs 19.99 PKR</p>
-                  
+                <div className="card-body card-body-custom">
+                  <h2 className="card-title mb-4">
+                    Explore Our Premium T-shirt Range!
+                  </h2>
+                  <p className="card-text mb-5">
+                    Step into unparalleled comfort and style with our T-shirts,
+                    blending top-notch quality with affordable prices. Each
+                    design is a statement, reflecting your unique taste. Explore
+                    our collection now and redefine your wardrobe!
+                  </p>
+                  <button
+                    type="button"
+                    className="btn btn-custom btn-outline-secondary"
+                  >
+                    SHOP NOW
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      <div className="container contact-form-container">
+        <h2>Contact Us</h2>
+        <form>
+          <div className="row">
+            <div className="col-md-6 mb-2">
+              <input type="text" className="form-control" placeholder="Name" />
+            </div>
+            <div className="col-md-6 mb-2">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email *"
+              />
+            </div>
+          </div>
+          <div className="mb-3">
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Phone number"
+              max={11}
+            />
+          </div>
+          <div className="mb-3">
+            <textarea className="form-control" placeholder="Comment"></textarea>
+          </div>
+          <button type="submit" className="btn btn-light mt-4">
+            Send
+          </button>
+        </form>
+      </div>
+{/* 
+      <div className="container mt-4 sub-to-emails">
+        <h4>Subscribe to our emails</h4>
+        <div className="row mt-3">
+          <div className="col-12">
+            <div className="input-container">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter your email"
+              />
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
